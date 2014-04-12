7d0f4ddb16bac91c76621ad6383e1f165d16a154
Mahbub Rony <mahbubur@nascenia.com>
2014-04-12 23:26:27 +0600

    'strating'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crecent::Application.config.secret_key_base = '45134890799ce5f482bcbab814653033b1f9b0094364b9bf276d08845982315371f56bcb5caa54c606a75570dd1ec3ddf37b2c436b7d37bef56b170658909dcc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Crecent::Application.config.session_store :cookie_store, key: '_crecent_session'

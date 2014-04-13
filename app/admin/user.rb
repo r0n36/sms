99b937e6390c91b9520168c65fa5f3891dffb475
imtiaz <imtiaz.emu.it@gmail>
2014-04-13 23:07:28 +0600

    admin user with new user create form

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,17 +1,26 @@
 ActiveAdmin.register User do
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+  menu :label => "Users"
+  index do
+    column 'Name' do |user|
+      link_to user.name, admin_user_path(user)
+    end
+    column 'Email' do |user|
+      link_to user.email, admin_user_path(user)
+    end
+    column 'Address' do |user|
+      link_to user.address, admin_user_path(user)
+    end
+    column 'Phone' do |user|
+      link_to user.phone, admin_user_path(user)
+    end
+    column 'Role' do |user|
+      link_to user.role, admin_user_path(user)
+    end
+    column 'Store' do |user|
+      link_to user.store_id, admin_user_path(user)
+    end
+  end
+  form :partial => 'form'
   
 end

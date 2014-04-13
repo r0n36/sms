cda33adb35629c63c520cf4efcf17d2d05f3e6cb
Mahbub Rony <mahbubur@nascenia.com>
2014-04-13 13:48:30 +0600

    All 4 modle and scaffolded

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Crecent::Application.routes.draw do
+  resources :reports
+
+  resources :users
+
+  resources :stores
+
+  resources :products
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
